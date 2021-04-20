e0112f564d53b9beaab202a58e4bda97f07e45c0
Oriel <orielzeus@github.com>
2021-04-20 16:55:58 -0400

    Primer comentario

---
+++ hello.f90
@@ -0,0 +1,6 @@
+program hello
+    character(20):myname
+    write *,"Ingrese nombre"
+    read *, myname
+    write *,"Hello ",myname
+end program hello
---
+++ suma.f90
@@ -0,0 +1,4 @@
+program suma 
+    integer::x(5)=(/4,8,7,3,5/)
+    print *,sum(x)
+end program suma
